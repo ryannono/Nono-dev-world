125995c6d227f8a19806f4774d99e335e2eedcb4
Ryan Nono <79291357+ryannono@users.noreply.github.com>
2022-11-09 19:26:10 -0500

    update logic

--- Python-Projects/roman_numerals_converter.py
+++ Python-Projects/roman_numerals_converter.py
@@ -26,20 +26,23 @@ for index, char in enumerate(s):
     if next_index < length:
         
         next_char = s[index+1]
-        
-        if (char == "I" or char == "i") and (next_index < length) and (next_char == "V" or next_char == "v"):
+        if (char == "I" or char == "i") and (next_char == "V" or next_char == "v"):
             add = add - myDic[char]
-        elif (char == "I" or char == "i") and (next_index < length) and (next_char == "X" or next_char == "v"):
+        elif (char == "I" or char == "i") and (next_char == "X" or next_char == "v"):
             add = add - myDic[char]
-        elif (char == "X" or char == "x") and (next_index < length) and (next_char == "L" or next_char == "l"):
+        elif (char == "X" or char == "x") and (next_char == "L" or next_char == "l"):
             add = add - myDic[char]
-        elif (char == "X" or char == "x") and (next_index < length) and (next_char == "C" or next_char == "c"):
+        elif (char == "X" or char == "x") and (next_char == "C" or next_char == "c"):
             add = add - myDic[char]
-        elif (char == "C" or char == "c") and (next_index < length) and (next_char == "D" or next_char == "d"):
+        elif (char == "C" or char == "c") and (next_char == "D" or next_char == "d"):
             add = add - myDic[char]
-        elif (char == "C" or char == "c") and (next_index < length) and (next_char == "M" or next_char == "m"):
+        elif (char == "C" or char == "c") and (next_char == "M" or next_char == "m"):
             add = add - myDic[char]
+        else:
+            add = add + myDic[char]
+
     else:
         add = add + myDic[char]
+        
         
 print("\nYour roman numeral in regular int format is: ",add,'\n') 
