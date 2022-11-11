6ef84bf4bd5163ed5967963caa939eb5a10c1cfe
Ryan Nono <79291357+ryannono@users.noreply.github.com>
2022-11-11 02:49:32 -0500

    test

---
+++ script.js
@@ -0,0 +1,92 @@
+// timeConversion converts a AM/PM time
+// to its military equivalent
+// requires s is a valid time string of format
+// hh:mm:ssAM or hh:mm:ssPM 
+function timeConversion(s) {
+    // get selection element
+    var selection = document.getElementById("AM_PM");
+    // count number of hour digits were inputted
+    var hour_digit_count = 0;
+    for (var i = 0; s[i] != " "; i++) {
+        hour_digit_count++;
+    }
+    // If time was in pm and not 12
+    // convert the time to military
+    if (selection.value === "PM" && s.substring(0, 2) != "12") {
+        // build hours
+        if (hour_digit_count === 2) {
+            var hours = Number(s.substring(0, 2));
+        }
+        else {
+            var hours = Number(s.substring(0, 1));
+        }
+        // increment by 12 for military time conversion
+        hours += 12;
+        // create new string
+        // if only 1 hour digit was entered
+        if (hour_digit_count != 2) {
+            s = hours + s.substring(1);
+        }
+        // if a number with 
+        else {
+            s = hours + s.substring(2);
+        }
+    }
+    // fix user input if hours did not have 2 digits
+    else if (hour_digit_count != 2) {
+        s = "0" + s;
+    }
+    return s;
+}
+// returns the passed input field id's
+// currenttly entered value as a string
+// requires id is a valid input field id
+function get_value(id) {
+    var input = document.getElementById(id);
+    var time_value = input.value;
+    return time_value;
+}
+// gets input field value and asigns its
+// conversion to the heading
+function assign_value() {
+    // converts time that is in the the input field
+    var intial_time = get_value("main_input");
+    var am_pm_element = document.getElementById("AM_PM");
+    var am_pm_value = am_pm_element.value;
+    var converted_time;
+    if (intial_time.length === 12) {
+        converted_time = timeConversion(intial_time);
+        // assigns converted time to heading
+        var military_time_element = document.getElementById("result_value");
+        military_time_element.innerHTML = converted_time;
+        // get rid of helper text
+        var helper_text_element = document.getElementById("result_helper_text");
+        helper_text_element.innerHTML = intial_time + " " + am_pm_value + " in military time is:";
+    }
+}
+function auto_format() {
+    var input_element = document.getElementById("main_input");
+    var input_string = input_element.value;
+    var len = input_string.length;
+    var new_value;
+    // if input field is empty
+    if (len === 0) {
+        // show helper text
+        var helper_text_element = document.getElementById("result_helper_text");
+        helper_text_element.innerHTML = "Please enter a time for conversion";
+    }
+    else if (len === 3 || len === 8) {
+        new_value = input_string.substring(0, len - 1) + " : " + input_string[len - 1];
+        input_element.value = new_value;
+    }
+}
+// auto format user input on keyup
+var input = document.getElementById("main_input");
+input.addEventListener("keydown", auto_format);
+input.addEventListener("emptied", auto_format);
+// button event listner 
+// on click the value in the input field gets converted
+// and assigned to the heading
+var button = document.getElementById("converter");
+button.addEventListener("click", assign_value);
+//# sourceMappingURL=script.js.map
