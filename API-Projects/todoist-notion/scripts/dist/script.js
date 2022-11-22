4e04d64ed03a760f3ae1be4766049496e81d4e8e
Ryan Nono <79291357+ryannono@users.noreply.github.com>
2022-11-22 18:15:33 -0500

    Notion can now create todoist tasks
    Both notion and Todoist can now append each other with tasks

--- API-Projects/todoist-notion/scripts/dist/script.js
+++ API-Projects/todoist-notion/scripts/dist/script.js
@@ -35,31 +35,34 @@ function newNotionTask(todoistTask) {
                                 }
                             }]
                     },
-                    "Due date": {
+                    "Due": {
                         "date": {
                             "start": dueDate.date
                         }
                     },
-                    "ID": {
-                        "number": Number(todoistTask.id)
+                    "TodoistID": {
+                        "rich_text": [{
+                                "type": "text",
+                                "text": {
+                                    "content": todoistTask.id
+                                }
+                            }]
                     },
                     "Status": {
                         "checkbox": todoistTask.isCompleted
                     },
                     "URL": {
                         "url": todoistTask.url
+                    },
+                    "Description": {
+                        "rich_text": [{
+                                "type": "text",
+                                "text": {
+                                    "content": todoistTask.description
+                                }
+                            }]
                     }
-                },
-                "children": [{
-                        "object": "block",
-                        "paragraph": {
-                            "rich_text": [{
-                                    "text": {
-                                        "content": todoistTask.description
-                                    }
-                                }]
-                        }
-                    }]
+                }
             });
         }
         else {
@@ -76,7 +79,7 @@ function newNotionTask(todoistTask) {
                                 }
                             }]
                     },
-                    "ID": {
+                    "TodoistID": {
                         "number": Number(todoistTask.id)
                     },
                     "Status": {
@@ -84,31 +87,29 @@ function newNotionTask(todoistTask) {
                     },
                     "URL": {
                         "url": todoistTask.url
+                    },
+                    "Description": {
+                        "rich_text": [{
+                                "type": "text",
+                                "text": {
+                                    "content": todoistTask.description
+                                }
+                            }]
                     }
                 },
-                "children": [{
-                        "object": "block",
-                        "paragraph": {
-                            "rich_text": [{
-                                    "text": {
-                                        "content": todoistTask.description
-                                    }
-                                }]
-                        }
-                    }]
             });
         }
     });
 }
-function IDSearchNotion(ID) {
+function IDSearchNotion(todoistID) {
     return __awaiter(this, void 0, void 0, function* () {
         const searchResults = yield notionApi.databases.query({
             database_id: databaseId,
             filter: {
                 and: [{
-                        property: "ID",
+                        property: "TodoistID",
                         number: {
-                            equals: ID
+                            equals: todoistID
                         }
                     }]
             }
@@ -119,19 +120,21 @@ function IDSearchNotion(ID) {
         return true;
     });
 }
-function notionUpToDateCheck(lastCheckedIndex) {
+function notionUpToDateCheck(lastCheckedTodoistIndex) {
     return __awaiter(this, void 0, void 0, function* () {
         const taskList = yield todoistApi.getTasks({
             filter: "created after: -24hours"
         });
         let latestElement = taskList[taskList.length - 1];
-        if (latestElement != null) {
+        console.log(latestElement);
+        if (latestElement != undefined) {
             let upToDate = yield IDSearchNotion(Number(latestElement.id));
             if (upToDate === false) {
-                for (let i = lastCheckedIndex + 1; i < taskList.length; i++) {
+                for (let i = lastCheckedTodoistIndex; i < taskList.length; i++) {
+                    console.log(i);
                     const todoistTask = taskList[i];
-                    const ID = Number(todoistTask.id);
-                    const notionSearchResult = yield IDSearchNotion(ID);
+                    const todoistID = Number(todoistTask.id);
+                    const notionSearchResult = yield IDSearchNotion(todoistID);
                     if (notionSearchResult === false) {
                         newNotionTask(todoistTask);
                     }
@@ -144,8 +147,115 @@ function notionUpToDateCheck(lastCheckedIndex) {
         return taskList.length - 1;
     });
 }
-let latestIndex = 0;
-setInterval(() => {
-    notionUpToDateCheck(latestIndex)
-        .then((value) => latestIndex = value);
-}, 2000);
+function objectToMap(object) {
+    const keys = Object.keys(object);
+    const map = new Map();
+    for (let i = 0; i < keys.length; i++) {
+        map.set(keys[i], object[keys[i]]);
+    }
+    return map;
+}
+function getNotionTitleProperty(pageObject) {
+    let propertiesObject = pageObject.properties;
+    let map = objectToMap(propertiesObject);
+    let titleobject = map.get("Task").title;
+    let text = objectToMap(objectToMap(titleobject).get("0")).get("plain_text");
+    return text;
+}
+function getNotionDescriptionProperty(pageObject) {
+    let propertiesObject = pageObject.properties;
+    let map = objectToMap(propertiesObject);
+    let richTextObject = map.get("Description").rich_text;
+    if (richTextObject.length === 0) {
+        return "";
+    }
+    let text = objectToMap(objectToMap(richTextObject).get("0")).get("plain_text");
+    return text;
+}
+function getNotionDueProperty(pageObject) {
+    let propertiesObject = pageObject.properties;
+    let map = objectToMap(propertiesObject);
+    let dateObject = map.get("Due").date;
+    if (dateObject === null) {
+        return "";
+    }
+    let date = objectToMap(dateObject).get("start");
+    return date;
+}
+function getNotionTodoistIDProperty(pageObject) {
+    let propertiesObject = pageObject.properties;
+    let map = objectToMap(propertiesObject);
+    let number = map.get("TodoistID").number;
+    return (String(number) === "null") ? "" : String(number);
+}
+function getNotionTodoistURLProperty(pageObject) {
+    let propertiesObject = pageObject.properties;
+    let map = objectToMap(propertiesObject);
+    let richTextObject = map.get("URL").rich_text;
+    if (richTextObject.length === 0) {
+        return "";
+    }
+    let url = objectToMap(objectToMap(richTextObject).get("0")).get("plain_text");
+    return url;
+}
+function newTodoistTask(notionPageObject) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let notionTitle = getNotionTitleProperty(notionPageObject);
+        let notionDescription = getNotionDescriptionProperty(notionPageObject);
+        let notionDue = getNotionDueProperty(notionPageObject);
+        let newTask = yield todoistApi.addTask({
+            content: notionTitle,
+            description: notionDescription,
+            dueDate: notionDue
+        });
+        return newTask;
+    });
+}
+function todoistUpToDateCheck(lastCheckedNotionIndex) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let timeWindow = new Date;
+        timeWindow.setHours(timeWindow.getHours() - 24);
+        let isoTimeWindow = timeWindow.toISOString();
+        const queryResponse = yield notionApi.databases.query({
+            database_id: databaseId,
+            filter: {
+                "timestamp": "created_time",
+                "created_time": {
+                    "after": isoTimeWindow
+                }
+            }
+        });
+        let taskList = queryResponse.results;
+        let latestElement = taskList[taskList.length - 1];
+        if (latestElement != undefined) {
+            for (let i = lastCheckedNotionIndex; i < taskList.length; i++) {
+                const element = taskList[i];
+                let todoistID = getNotionTodoistIDProperty(element);
+                if (todoistID.length === 0) {
+                    let newTask = yield newTodoistTask(element);
+                    let createdTodoistID = newTask.id;
+                    let createdTodoistURL = newTask.url;
+                    let currentNotionPageId = element.id;
+                    notionApi.pages.update({
+                        page_id: currentNotionPageId,
+                        properties: {
+                            "TodoistID": {
+                                "number": Number(createdTodoistID)
+                            },
+                            "URL": {
+                                "url": createdTodoistURL
+                            }
+                        }
+                    });
+                    if (i === taskList.length - 1) {
+                        return i;
+                    }
+                }
+            }
+        }
+        return taskList.length - 1;
+    });
+}
+let latestNotionIndex = 0;
+let latestTodoistIndex = 0;
+todoistUpToDateCheck(latestNotionIndex);
--- API-Projects/todoist-notion/scripts/src/script.ts
+++ API-Projects/todoist-notion/scripts/src/script.ts
@@ -1,8 +1,9 @@
 
 import dotenv = require("dotenv"); // key environment
-import { Task, TodoistApi } from "@doist/todoist-api-typescript"; // todoist api
+import { DueDate, Task, TodoistApi } from "@doist/todoist-api-typescript"; // todoist api
 import { Client } from "@notionhq/client"; // notion api
-import { QueryDatabaseResponse } from "@notionhq/client/build/src/api-endpoints";
+import { getPageProperty, GetPagePropertyResponse, DatePropertyItemObjectResponse, NumberPropertyItemObjectResponse, PageObjectResponse, PartialPageObjectResponse, PropertyItemListResponse, PropertyItemObjectResponse, QueryDatabaseResponse, RichTextItemResponse, RichTextPropertyItemObjectResponse, TextRichTextItemResponse, TitlePropertyItemObjectResponse, UrlPropertyItemObjectResponse } from "@notionhq/client/build/src/api-endpoints";
+import { stringify } from "querystring";
 
 // get keys from environment
 dotenv.config();
@@ -32,7 +33,7 @@ async function newNotionTask(todoistTask: Task): Promise<void> {
             },
     
             "properties": {
-    
+
                     "Task": {
                         "title": [{
                             "text": { 
@@ -40,35 +41,36 @@ async function newNotionTask(todoistTask: Task): Promise<void> {
                             }
                         }]
                     },
-                    "Due date":{
+                    "Due":{
                         "date" : {
                             "start" : dueDate.date
                         }
                     },
-                    "ID":{
-                        "number" : Number(todoistTask.id)
+                    "TodoistID": {
+                        "rich_text": [{
+                            "type" : "text",
+                            "text": {
+                                "content" : todoistTask.id
+                            }
+                        }]
                     },
                     "Status":{
                         "checkbox" : todoistTask.isCompleted
                     },
                     "URL": {
                         "url": todoistTask.url
+                    },
+                    "Description": {
+                        "rich_text": [{
+                            "type" : "text",
+                            "text": {
+                                "content" : todoistTask.description
+                            }
+                        }]
                     }
                     
-    
-            },
-    
-            "children": [{
-                "object": "block",
-                "paragraph": {
-                    "rich_text": [{
-                        "text": {
-                            "content": todoistTask.description
-                        }
-                    }]
-                }
-            }]
-    
+            }
+
         });
     }
     else{
@@ -88,7 +90,7 @@ async function newNotionTask(todoistTask: Task): Promise<void> {
                             }
                         }]
                     },
-                    "ID":{
+                    "TodoistID":{
                         "number" : Number(todoistTask.id)
                     },
                     "Status":{
@@ -96,21 +98,17 @@ async function newNotionTask(todoistTask: Task): Promise<void> {
                     },
                     "URL": {
                         "url": todoistTask.url
+                    },
+                    "Description": {
+                        "rich_text": [{
+                            "type" : "text",
+                            "text": {
+                                "content" : todoistTask.description
+                            }
+                        }]
                     }
                     
-    
             },
-    
-            "children": [{
-                "object": "block",
-                "paragraph": {
-                    "rich_text": [{
-                        "text": {
-                            "content": todoistTask.description
-                        }
-                    }]
-                }
-            }]
     
         });
     }
@@ -118,15 +116,15 @@ async function newNotionTask(todoistTask: Task): Promise<void> {
 
 // searchNotion queries notion for an ID and returns
 // true if an element was found with the ID and false if not
-async function IDSearchNotion(ID:number): Promise<boolean> {
+async function IDSearchNotion(todoistID:number): Promise<boolean> {
     
     const searchResults: QueryDatabaseResponse = await notionApi.databases.query({
         database_id: databaseId,
         filter: {
             and: [{
-                property: "ID",
+                property: "TodoistID",
                 number: {
-                    equals: ID
+                    equals: todoistID
                 }
             }]
         }
@@ -143,18 +141,18 @@ async function IDSearchNotion(ID:number): Promise<boolean> {
 
 // notionUpToDateCheck checks if notion has the latest
 // todoist tasks in its database. If it doesnt they are added.
-async function notionUpToDateCheck(lastCheckedIndex: number): Promise<number> {
+async function notionUpToDateCheck(lastCheckedTodoistIndex: number): Promise<number> {
     
     // get list of todoist tasks created today
     const taskList:Array<Task> = await todoistApi.getTasks({
         filter: "created after: -24hours"
     });
-    //console.log(taskList);
 
     // get newest (last element in tasklist) task in
     // todoist and check if it is in notion
     let latestElement:Task = taskList[taskList.length-1];
-    if (latestElement != null) {
+    console.log(latestElement);
+    if (latestElement != undefined) {
 
         let upToDate:boolean = await IDSearchNotion(Number(latestElement.id));
         // if task was not found then notion is not up to
@@ -162,11 +160,11 @@ async function notionUpToDateCheck(lastCheckedIndex: number): Promise<number> {
         // ones that aren't in notion yet
         if (upToDate === false) {
             
-            for (let i = lastCheckedIndex+1; i < taskList.length; i++) {
-                
+            for (let i = lastCheckedTodoistIndex; i < taskList.length; i++) {
+                console.log(i);
                 const todoistTask: Task = taskList[i];
-                const ID:number = Number(todoistTask.id);
-                const notionSearchResult:boolean = await IDSearchNotion(ID);
+                const todoistID:number = Number(todoistTask.id);
+                const notionSearchResult:boolean = await IDSearchNotion(todoistID);
                 
                 if (notionSearchResult === false) {
                     newNotionTask(todoistTask);
@@ -179,16 +177,175 @@ async function notionUpToDateCheck(lastCheckedIndex: number): Promise<number> {
         }
     }
     
-    return taskList.length-1;
+    return taskList.length-1; // no element in list
+}
 
+// objectToMap takes in a object and returns it in a map format
+function objectToMap(object: any): Map<any,any>{
+    const keys = Object.keys(object);
+    const map = new Map();
+    for (let i = 0; i < keys.length; i++) {
+        
+        map.set(keys[i], object[keys[i]]);
+    }
+    return map;
+}
+
+// getNotionTitleProperty return notions title
+// property for the passed page
+function getNotionTitleProperty(pageObject: PageObjectResponse){
+    let propertiesObject = pageObject.properties as object;
+    let map = objectToMap(propertiesObject);
+    let titleobject = map.get("Task").title as object;
+    let text = objectToMap(objectToMap(titleobject).get("0")).get("plain_text");
+    return text;
+}
+
+// getNotionDescriptionProperty return notions description
+// property for the passed page
+function getNotionDescriptionProperty(pageObject: PageObjectResponse){
+    let propertiesObject = pageObject.properties as object;
+    let map = objectToMap(propertiesObject);
+    let richTextObject = map.get("Description").rich_text as Array<any>;
+    if (richTextObject.length === 0) {
+        return "";
+    }
+    let text = objectToMap(objectToMap(richTextObject).get("0")).get("plain_text");
+    return text
+}
+
+// getNotionDueProperty return notions due
+// property for the passed page
+function getNotionDueProperty(pageObject: PageObjectResponse){
+    let propertiesObject = pageObject.properties as object;
+    let map = objectToMap(propertiesObject);
+    let dateObject = map.get("Due").date as object;
+    if (dateObject === null) {
+        return "";
+    }
+    let date = objectToMap(dateObject).get("start");
+    return date;
+}
+
+// getNotionTodoistIDProperty return notions TodoistID
+// property for the passed page
+function getNotionTodoistIDProperty(pageObject: PageObjectResponse){
+    let propertiesObject = pageObject.properties as object;
+    let map = objectToMap(propertiesObject);
+    let number = map.get("TodoistID").number as object;
+    return (String(number) === "null") ? "" : String(number);
+}
+
+// getNotionTodoistURLProperty return notions URL
+// property for the passed page
+function getNotionTodoistURLProperty(pageObject: PageObjectResponse){
+    let propertiesObject = pageObject.properties as object;
+    let map = objectToMap(propertiesObject);
+    let richTextObject = map.get("URL").rich_text as Array<any>;
+    if (richTextObject.length === 0) {
+        return "";
+    }
+    let url = objectToMap(objectToMap(richTextObject).get("0")).get("plain_text");
+    return url
+}
+
+// newTodoistTask creates a new todoist task with all the notion values
+async function newTodoistTask(notionPageObject: PageObjectResponse) {
+    
+    let notionTitle = getNotionTitleProperty(notionPageObject);
+    let notionDescription = getNotionDescriptionProperty(notionPageObject);
+    let notionDue = getNotionDueProperty(notionPageObject);
+
+    let newTask = await todoistApi.addTask({
+        content: notionTitle,
+        description: notionDescription,
+        dueDate: notionDue
+    })
+
+    return newTask;
+}
+
+// todoistUpToDateCheck check if all new tasks from notion are present in todoist
+// if not it adds them. The funtion also adds todoist's ID information on to the 
+// notion database once the new task is created.
+async function todoistUpToDateCheck(lastCheckedNotionIndex: number){
+
+    // get time 24 hours ago
+    let timeWindow: Date = new Date;
+    timeWindow.setHours(timeWindow.getHours() - 24);
+    let isoTimeWindow: string = timeWindow.toISOString();
+
+    // query notion database with past 24 hours filter
+    const queryResponse: QueryDatabaseResponse = await notionApi.databases.query({
+        database_id: databaseId,
+        filter: {
+                "timestamp": "created_time",
+                "created_time": {
+                        "after": isoTimeWindow
+                }
+        }
+    });
+
+    // declare info on notion task list
+    let taskList = queryResponse.results as Array<PageObjectResponse>;
+    let latestElement = taskList[taskList.length-1] as PageObjectResponse;
+
+    // when a latest element exists
+    if (latestElement != undefined) {
+        
+        for (let i = lastCheckedNotionIndex; i < taskList.length; i++) {
+            const element = taskList[i];
+
+            let todoistID = getNotionTodoistIDProperty(element);
+            
+            // if notion task doesn't have an associated todoist ID
+            // then it hasn't been synced to TodoIst yet
+            if (todoistID.length === 0) {
+
+                // create new Todoist task
+                let newTask: Task = await newTodoistTask(element)
+                    
+                // update notion task to have todoist id and url
+                let createdTodoistID = newTask.id;
+                let createdTodoistURL = newTask.url;
+                let currentNotionPageId = element.id;
+
+                notionApi.pages.update({
+                    page_id: currentNotionPageId,
+                    properties: {
+                        "TodoistID": {
+                            "number": Number(createdTodoistID)
+                        },
+                        "URL": {
+                            "url" : createdTodoistURL
+                        }
+                    }
+                })
+
+                if (i === taskList.length-1) {
+                    return i;
+                }
+
+            }
+
+
+        }
+    }
+
+    return taskList.length-1;
 }
 
 
-let latestIndex:number = 0;
+let latestNotionIndex:number = 0;
+let latestTodoistIndex:number = 0;
+todoistUpToDateCheck(latestNotionIndex)
+// setInterval(() => {
+//     notionUpToDateCheck(latestNotionIndex)
+//         .then((value) => latestNotionIndex = value)
+// }, 2000)
 
-setInterval(() => {
-    notionUpToDateCheck(latestIndex)
-        .then((value) => latestIndex = value)
-}, 2000)
-
+// setInterval(() => {
+//     todoistUpToDateCheck(latestTodoistIndex)
+//         .then((value) => latestTodoistIndex = value)
+// }, 2000)
 
