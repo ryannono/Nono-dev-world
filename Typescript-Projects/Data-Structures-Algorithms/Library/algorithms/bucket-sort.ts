048f4516ae82cb103035f6d1d0181e3b0574e827
Ryan Nono <ryan_nono@hotmail.com>
2023-02-20 13:41:08 -0500

    modified:   Typescript-Projects/Data-Structures-Algorithms/build/Library/functions/comparator.d.ts

--- Typescript-Projects/Data-Structures-Algorithms/Library/algorithms/bucket-sort.ts
+++ Typescript-Projects/Data-Structures-Algorithms/Library/algorithms/bucket-sort.ts
@@ -1,41 +1,69 @@
 // ----------------- Imports ----------------- //
 
-import {quickSort} from './quick-sort';
 import {Comparator, defaultComparator} from '../functions/comparator';
 import {NumKeyAccessor, defaultNumKeyAccessor} from '../functions/keyAccessor';
+import {insertionSort} from './insertion-sort';
+import {mergeSort} from './merge-sort';
 
 // ---------------- Bucket Sort ---------------- //
 
 /**
- * We create an array of arrays, where each array is a bucket. We then iterate through the input array
- * and push each element into the bucket that corresponds to its key. Finally, we iterate through the
- * buckets and sort each bucket using quick sort
- * @param {T[]} array - The array to sort.
- * @param {number} maxKey - The maximum value of the key.
- * @param keyAccessor - A function that takes in an element and returns a number. This number is used
- * to determine which bucket the element should be placed in.
- * @param comparator - A function that takes two elements and returns a number. If the number is less
- * than 0, the first element is less than the second. If the number is greater than 0, the first
- * element is greater than the second. If the number is 0, the two elements are equal.
+ * We create an array of buckets, each bucket is an array of elements. We then iterate through the
+ * array and place each element into the bucket corresponding to its key. We then sort each bucket and
+ * concatenate the buckets together
+ * @param {T[]} array - The array to be sorted.
+ * @param {number} [minKey] - The minimum key value in the array.
+ * @param {number} [maxKey] - The maximum key value in the array.
+ * @param keyAccessor - A function that takes in an element and returns a number.
+ * @param comparator - A function that compares two elements and returns a number.
  * @returns The sorted array.
  */
 function bucketSort<T>(
   array: T[],
-  maxKey: number,
+  minKey?: number,
+  maxKey?: number,
   keyAccessor: NumKeyAccessor<T> = defaultNumKeyAccessor,
   comparator: Comparator<T> = defaultComparator
 ) {
-  const containerArray: T[][] = [];
+  if (!array.length) return array;
 
-  while (maxKey-- + 1) containerArray.push([]);
+  const getFlooredKey = (element: T) => Math.floor(keyAccessor(element));
 
-  while (array.length) {
-    const currElement = array.pop()!;
-    containerArray[keyAccessor(currElement)].push(currElement);
+  /* If the user does not specify the max and min key,
+  we find the max and min key by iterating through the array. */
+  if (typeof maxKey === 'undefined' || typeof minKey === 'undefined') {
+    let tempMax, tempMin;
+    tempMax = tempMin = array[0];
+
+    for (const element of array) {
+      if (comparator(element, tempMax) > 0) tempMax = element;
+      if (comparator(element, tempMin) < 0) tempMin = element;
+    }
+
+    maxKey = getFlooredKey(tempMax);
+    minKey = getFlooredKey(tempMin);
+  } else {
+    minKey = Math.floor(minKey);
+    maxKey = Math.floor(maxKey);
   }
 
+  let keyRange = maxKey - minKey + 1;
+  const offset = -minKey;
+  const containerArray: T[][] = [];
+
+  /* Creating an array of empty arrays. */
+  while (keyRange--) containerArray.push([]);
+
+  /* Iterating through the array and placing each element into the bucket corresponding to its key. */
+  while (array.length) {
+    const currElement = array.pop()!;
+    containerArray[getFlooredKey(currElement) + offset].push(currElement);
+  }
+
+  /* Sorting the buckets. */
   for (const bucket of containerArray) {
-    quickSort(bucket, 0, bucket.length - 1, comparator);
+    if (bucket.length < 20) insertionSort(bucket, 0, comparator);
+    else mergeSort(bucket);
     array.push(...bucket);
   }
 
--- Typescript-Projects/Data-Structures-Algorithms/build/Library/algorithms/bucket-sort.js
+++ Typescript-Projects/Data-Structures-Algorithms/build/Library/algorithms/bucket-sort.js
@@ -1,33 +1,66 @@
 "use strict";
 // ----------------- Imports ----------------- //
 Object.defineProperty(exports, "__esModule", { value: true });
-const quick_sort_1 = require("./quick-sort");
 const comparator_1 = require("../functions/comparator");
 const keyAccessor_1 = require("../functions/keyAccessor");
+const insertion_sort_1 = require("./insertion-sort");
+const merge_sort_1 = require("./merge-sort");
 // ---------------- Bucket Sort ---------------- //
 /**
- * We create an array of arrays, where each array is a bucket. We then iterate through the input array
- * and push each element into the bucket that corresponds to its key. Finally, we iterate through the
- * buckets and sort each bucket using quick sort
- * @param {T[]} array - The array to sort.
- * @param {number} maxKey - The maximum value of the key.
- * @param keyAccessor - A function that takes in an element and returns a number. This number is used
- * to determine which bucket the element should be placed in.
- * @param comparator - A function that takes two elements and returns a number. If the number is less
- * than 0, the first element is less than the second. If the number is greater than 0, the first
- * element is greater than the second. If the number is 0, the two elements are equal.
+ * We create an array of buckets, each bucket is an array of elements. We then iterate through the
+ * array and place each element into the bucket corresponding to its key. We then sort each bucket and
+ * concatenate the buckets together
+ * @param {T[]} array - The array to be sorted.
+ * @param {number} [minKey] - The minimum key value in the array.
+ * @param {number} [maxKey] - The maximum key value in the array.
+ * @param keyAccessor - A function that takes in an element and returns a number.
+ * @param comparator - A function that compares two elements and returns a number.
  * @returns The sorted array.
  */
-function bucketSort(array, maxKey, keyAccessor = keyAccessor_1.defaultNumKeyAccessor, comparator = comparator_1.defaultComparator) {
+function bucketSort(array, minKey, maxKey, keyAccessor = keyAccessor_1.defaultNumKeyAccessor, comparator = comparator_1.defaultComparator) {
+    if (!array.length)
+        return array;
+    /**
+     * It takes an array of elements, and returns an array of elements grouped by the floored value of the
+     * key accessor function
+     * @param {T} element - T
+     */
+    const getFlooredKey = (element) => Math.floor(keyAccessor(element));
+    /* If the user does not specify the max and min key,
+    we find the max and min key by iterating through the array. */
+    if (typeof maxKey === 'undefined' || typeof minKey === 'undefined') {
+        let tempMax, tempMin;
+        tempMax = tempMin = array[0];
+        for (const element of array) {
+            if (comparator(element, tempMax) > 0)
+                tempMax = element;
+            if (comparator(element, tempMin) < 0)
+                tempMin = element;
+        }
+        maxKey = getFlooredKey(tempMax);
+        minKey = getFlooredKey(tempMin);
+    }
+    else {
+        minKey = Math.floor(minKey);
+        maxKey = Math.floor(maxKey);
+    }
+    let keyRange = maxKey - minKey + 1;
+    const offset = -minKey;
     const containerArray = [];
-    while (maxKey-- + 1)
+    /* Creating an array of empty arrays. */
+    while (keyRange--)
         containerArray.push([]);
+    /* Iterating through the array and placing each element into the bucket corresponding to its key. */
     while (array.length) {
         const currElement = array.pop();
-        containerArray[keyAccessor(currElement)].push(currElement);
+        containerArray[getFlooredKey(currElement) + offset].push(currElement);
     }
+    /* Sorting the buckets. */
     for (const bucket of containerArray) {
-        (0, quick_sort_1.quickSort)(bucket, 0, bucket.length - 1, comparator);
+        if (bucket.length < 20)
+            (0, insertion_sort_1.insertionSort)(bucket, 0, comparator);
+        else
+            (0, merge_sort_1.mergeSort)(bucket);
         array.push(...bucket);
     }
     return array;
